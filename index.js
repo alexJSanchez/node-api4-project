3f8b306adf5f9efe001389dd662f9be99bb8dc13
alex sanchez <alexjsanchez1020@gmail.com>
2022-12-12 10:45:28 -0500

    add server listen

--- index.js
+++ index.js
@@ -1,2 +1,24 @@
 require('dotenv').config()
-console.log(process.env)
+// console.log(process.env)
+const express = require('express')
+const cors = require('cors')
+
+const server = express()
+const PORT = process.env.port || 9000;
+server.use(express.json())
+server.use(cors())
+
+server.get('/api' , (req,res)=>{
+    res.json({
+        nessage: 'it is working'
+    })
+ })
+
+server.use('*' , (req,res)=>{
+    res.send(`<h1>hello world!</h1>`)
+})
+
+server.listen( PORT ,  ()=>{
+    console.log('server is runing')
+})
+console.log(process.env.PORT, process.env.NODE_ENV)  
